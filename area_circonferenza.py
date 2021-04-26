843ffbcb55a7f2983839b3794bdbb3da09b0b711
Andrrroo <andrea190504@gmail.com>
2021-04-26 11:50:51 +0200

    Done

--- area_circonferenza.py
+++ area_circonferenza.py
@@ -1,14 +1,14 @@
 import math
 
-raggio_1 = int(input("Inserisci valore raggio: "))
-
-def area_cerchio(raggio):
-    return round((math.pi * raggio), 2)
+def area_cerchio(r):
+    return round((math.pi * r), 2)
     
 
-def circonferenza_cerchio(raggio):
-    return round((2 * math.pi * raggio), 2)
+def circonferenza_cerchio(r):
+    return round((2 * math.pi * r), 2)
 
-print("L'area del cerchio è:", area_cerchio(raggio_1))
+raggio = int(input("Inserisci valore raggio: "))
 
-print("La circonferenza del cercho è:", circonferenza_cerchio(raggio_1))
+print("L'area del cerchio è:", area_cerchio(raggio))
+
+print("La circonferenza del cerchio è:", circonferenza_cerchio(raggio))
