774e51f7d9ffacd3f434d1318cc6505a2dab3560
Andrrroo <andrea190504@gmail.com>
2021-05-15 09:54:20 +0200

    somma

---
+++ somma_interi.py
@@ -0,0 +1,3 @@
+def somma_n(n1, n2, n3, n4):
+    return (int(n1) + int(n2) + int(n3) + int(n4))
+print(somma_n(input("inserisci un numero intero: "), input("inserisci un numero intero: "), input("inserisci un numero intero: "), input("inserisci un numero intero: ")))
