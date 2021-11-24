4d58be78d84a783435795c616d4176cd93a189d8
Andrrroo <andrea190504@gmail.com>
2021-11-24 20:17:44 +0100

    fatto

--- ruzzle/cerca.py
+++ ruzzle/cerca.py
@@ -1,12 +1,13 @@
 
 
 str= "pop"  
-it = 'words.italian.txt' 
+it = 'words.italian.txt'
 
 f = open(it, 'r')
 line = f.readline()
 
 for line in f:  
-    print(str) 
+    
     if str == line[:-1]:  
         print("vero")
+    
